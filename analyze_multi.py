8b264df97751a419cf12a48bbf219a543bb79651
Avash Shrestha <avash@stanford.edu>
2021-11-01 22:14:37 -0700

    rage

---
+++ analyze_multi.py
@@ -0,0 +1,38 @@
+import ast
+import json
+import math
+import pickle
+import random
+import time
+from time import gmtime, strftime
+from collections import namedtuple
+from datetime import date, datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pandas.io.parsers import read_csv
+import requests
+import seaborn as sns
+import base_script as bs
+import os
+
+
+def analyze():
+    filename = open("2shots_responses_2021-11-02T_04-48-12Z.txt", encoding="utf-8")
+    data = filename.readlines()
+    for line in data:
+        line = ast.literal_eval(line)
+        # print(line["completions"])
+        wtf = []
+        print(len(line["completions"][0]["data"]["tokens"][0]["topTokens"]))
+        for elem in line["completions"][0]["data"]["tokens"][0]["topTokens"]:
+            wtf.append((elem["token"], round(math.exp(elem["logprob"]), 3)))
+        total_prob = 0
+        for elem in wtf:
+            total_prob += elem[1]
+        print(wtf)
+        print(total_prob)
+
+
+analyze()
